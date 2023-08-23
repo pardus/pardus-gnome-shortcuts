c58e1b976888618e5833385a9803324d221f0197
osman <osman.coskun@pardus.org.tr>
2023-08-23 17:32:41 +0300

    feat: extension working logic changed.

--- pardus-gnome-shortcuts@dev.pardus.org.tr/extension.js
+++ pardus-gnome-shortcuts@dev.pardus.org.tr/extension.js
@@ -1,44 +1,27 @@
-// Example#8
-const { St, GObject, Gio, GLib } = imports.gi
-const { main, panelMenu, popupMenu } = imports.ui
+const { main , panelMenu } = imports.ui
+const { St, GObject, Gio, GLib, Clutter } = imports.gi
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
-let myPopup;
-function init() {
-}
-
-function enable() {
-    extensionMenu = new PardusGnomeShortcut();
-    main.panel.addToStatusArea('PardusGnomeShortcuts', extensionMenu, 1);
-}
-
-function disable() {
-    myPopup.destroy();
-}
-const PardusGnomeShortcut = GObject.registerClass(
-    class PardusGnomeShortcut extends panelMenu.Button {
-
+let pardusGnomeShortcutsIndicator;
+const PardusGnomeShortcutsIndicator = GObject.registerClass(
+    class PardusGnomeShortcutsIndicator extends panelMenu.Button {
         _init() {
             super._init(0);
             let icon = new St.Icon({
                 gicon: Gio.icon_new_for_string(Me.dir.get_path() + '/icon.svg'),
                 style_class: 'system-status-icon',
             });
-
             this.add_child(icon);
-
-
-            this.menu.addMenuItem(new popupMenu.PopupSeparatorMenuItem());
-
-            // image item
-            let shortcutMenuLauncher = new popupMenu.PopupImageMenuItem(
-                'Open Shortcuts',
-                'input-keyboard-symbolic',
-            );
-            shortcutMenuLauncher.connect("activate", () => {
-                GLib.spawn_command_line_async('firefox')
-            })
-            this.menu.addMenuItem(shortcutMenuLauncher);
+            this.connect('event', (actor, event) => {
+                if (event.type() === Clutter.EventType.BUTTON_PRESS)
+                    GLib.spawn_command_line_async('pardus-gnome-shortcuts')
+            });
         }
     });
-
+function enable() {
+    pardusGnomeShortcutsIndicator = new PardusGnomeShortcutsIndicator();
+    main.panel.addToStatusArea('pardusGnomeShortcutsIndicator', pardusGnomeShortcutsIndicator, 1);
+}
+function disable() {
+    pardusGnomeShortcutsIndicator.destroy();
+}
